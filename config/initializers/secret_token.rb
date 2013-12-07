76354501545459b073d8377668849925b130697a
Sue White <sue@mailworks.org>
2013-12-06 22:01:46 -0800

    added a staticpages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2a68d75e2088ea2231ff8c96b4b6087c05743081aca2ce0c9db2ca3d53c04fceb0a11a226b9195e8d3515aed8f52834bfcf22f87b3c5f426254a3eb73ca469d5'
